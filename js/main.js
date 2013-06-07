43064336b5e4644bdf29d7a134f1fe9d3ff40110
Zach <zachreinhardtguy@gmail.com>
2013-06-06 22:37:02 -0400

    Small work done

--- js/main.js
+++ js/main.js
@@ -6,3 +6,32 @@
 
 $(function(){
 });
+
+
+
+//Pos: [x, y] of top left corner
+/*function square(context, pos, width){
+	context.save();
+	context.fillStyle = 'black';
+	context.fillRect(pos[0], pos[1], width, width)
+	context.restore();
+}*/
+
+//Size: size of each square in grid
+function grid(width, height, size){
+	this.width = width;
+	this.height = height;
+	this.size = size;
+
+	this.createSquares = function(){
+		this.squares = [];
+		rows = int(size / this.height)
+		cols = int(size / this.width)
+	}
+}
+
+function square(pos, width){
+	this.pos = pos;
+	this.width = width;
+}
+
